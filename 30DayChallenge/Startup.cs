42da8e85cf4f9108039cf9d34278cc01a7bfe897
Chelsea Farley <chelseafarley91@gmail.com>
2021-11-11 20:37:28 +1300

    Adding stripe payment key

--- 30DayChallenge/Startup.cs
+++ 30DayChallenge/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using _30DayChallenge.Services;
+using Stripe;
 
 namespace _30DayChallenge
 {
